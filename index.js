c2bfab558b91ad5e904decb3cd46885cd95bfc13
adamboov <adamboov@63.com>
2019-08-29 15:32:41 +0800

    添加underscore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var url = require('url');
+var querystring = require('querystring');
 // 2.创建服务
 http.createServer(function(req, res) {
     //为res对象添加一个render()函数
@@ -42,22 +43,44 @@ http.createServer(function(req, res) {
     } else if (req.url === '/item' && req.method === 'get') {
         res.render(path.join(__dirname, 'views', 'item.html'));
     } else if (req.url.startsWith('/add') && req.method === 'get') {
-        var list = [];
-        list.push(urlObj.query);
-        fs.writeFile(path.join(__dirname, 'data', 'data.json'), JSON.stringify(list), function(err) {
-            if (err) {
+        fs.readFile(path.join(__dirname, 'data', 'data.json'), 'utf8', function(err, data) {
+            if (err && err.code !== 'ENOENT') {
                 throw err;
             }
-            console.log('ok');
-            // 设置响应报文头，告诉浏览器跳转页面
-            res.statusCode = 302;
-            res.statusMessage = 'Found';
-            res.setHeader('Location', '/');
-            res.end();
+            var list = JSON.parse(data || '[]');
+            list.push(urlObj.query);
+            fs.writeFile(path.join(__dirname, 'data', 'data.json'), JSON.stringify(list), function(err) {
+                if (err) {
+                    throw err;
+                }
+                console.log('ok');
+                // 设置响应报文头，告诉浏览器跳转页面
+                res.statusCode = 302;
+                res.statusMessage = 'Found';
+                res.setHeader('Location', '/');
+                res.end();
+            });
         });
         // res.render(path.join(__dirname, 'views', 'add.html'));
     } else if (req.url === '/add' && req.method === 'post') {
-        res.render(path.join(__dirname, 'views', 'add.html'));
+        fs.readFile(path.join(__dirname, 'data', 'data.json'), 'utf8', function(err, data) {
+            if (err && err.code !== 'ENOENT') {
+                throw err;
+            }
+            var list = JSON.parse(data || '[]');
+            var array = [];
+            req.on('data', function(chunk) {
+                array.push(chunk);
+            });
+            req.on('end', function() {
+                var postBody = Buffer.concat(array);
+                postBody = postBody.toString('utf8');
+                postBody = querystring.parse(postBody);
+                list.push(postBody);
+                res.end('over');
+            });
+        });
+        // res.render(path.join(__dirname, 'views', 'add.html'));
     } else if (req.url.startsWith('/resources') && req.method === 'get') {
         fs.readFile(path.join(__dirname, req.url), function(err, data) {
             if (err) {
